46932853da5620866b0639d2e20fdc57a10b40e3
RachelLindquist <firefly7491@gmail.com>
2022-06-12 16:27:53 -0400

    Finished Lab 5 pt 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int itemPrice = 99;
+        double shippingCost = 0;
+        double SHIPPING_PERCENT_COST = 0.02;
+        if (itemPrice < 100){
+            shippingCost = itemPrice * SHIPPING_PERCENT_COST;
+        }
+        double totalPrice = itemPrice+shippingCost;
+        System.out.println("For an item that costs " + itemPrice + " dollars, the shipping cost is " +
+                shippingCost + " dollars, for a total of " + totalPrice + " dollars");
+
+
+
     }
 }
